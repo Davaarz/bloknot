2c1696c01460b395155933dc215524cc88713949
David Arzymanyan <davedaplusa@mail.ru>
2021-11-02 19:48:00 +0500

    Форма оранж и длинная

--- Mynotepad/Mynotepad/Form1.Designer.cs
+++ Mynotepad/Mynotepad/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace Mynotepad
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 172);
+            this.button1.Location = new System.Drawing.Point(599, 183);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 53);
             this.button1.TabIndex = 0;
@@ -46,8 +46,8 @@ namespace Mynotepad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1317, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
